3a2d275b447c763c73d52672a7bfffc30918692b
APradel <antony.pradel@gmail.com>
2016-06-06 16:25:22 +0200

    Choix et visuel des templates

--- src/ScrumBoardItBundle/Controller/DefaultController.php
+++ src/ScrumBoardItBundle/Controller/DefaultController.php
@@ -91,8 +91,16 @@ class DefaultController extends Controller
         $service = $this->get($this->getUser()->getConnector() . '.api');
         $selected = $request->request->get('issues');
 
+        $session = $request->getSession();
+        $template = array(
+            'userStory' => $session->get('template')['userStory'],
+            'subTask' => $session->get('template')['subTask'],
+            'poc' => $session->get('template')['poc']
+        );
+
         return $this->render('ScrumBoardItBundle:Print:tickets.html.twig', array(
-            'issues' => $service->getSelectedIssues($request, $selected)
+            'issues' => $service->getSelectedIssues($request, $selected),
+            'template' => $template
         ));
     }
 
